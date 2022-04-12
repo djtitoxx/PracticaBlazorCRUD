1eb9347d8793900ff1b59e2bcb2451878b7965da
galacsoftwaredev <galac.softbb@galac.com>
2022-04-12 12:03:18 -0400

    Ejercicio CRUD en Blazor

---
+++ BlazorCRUD.UI/Interfaces/IFilmService.cs
@@ -0,0 +1,12 @@
+﻿using BlazorCrud.Model;
+
+namespace BlazorCRUD.UI.Interfaces
+{
+    public interface IFilmService
+    {
+        Task<IEnumerable<Film>> GetAllFilms();
+        Task<Film> GetDetails(int id);
+        Task<bool> DeleteFilm(int id);
+        Task<bool> SaveFilm(Film film);
+    }
+}
